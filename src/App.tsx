34bf7313c1ae9661d78c7818e202ccc5dcccd0b5
jayawickrama <nishantharaka2004@gmail.com>
2025-03-19 23:20:40 +0530

    creating navbar and their classes and navigators

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import {Route, Router, Routes} from "react-router-dom";
 import {Content} from "./Page/Content.tsx";
 import NavBar from "./Components/NavBar.tsx";
 import {Gallery} from "./Page/Gallery.tsx";
+import {Booking} from "./Page/Booking.tsx";
+import {Contact} from "./Page/Contact.tsx";
 
 function App() {
 
@@ -14,6 +16,9 @@ function App() {
                     <>
                             <Route path="/" element={<Content/> }/>
                             <Route path="/gallery" element={<Gallery/> }/>
+                            <Route path="/booking" element={<Booking/> }/>
+                            <Route path="/contact" element={<Contact/> }/>
+                            <Route path="/services" element={}/>
                     </>
                   </Routes>
             </NavBar>
