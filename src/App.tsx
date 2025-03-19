bdc411da5bc275b3c849223bea675984d28e81b0
jayawickrama <nishantharaka2004@gmail.com>
2025-03-19 23:31:04 +0530

    created Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,29 @@
 import './App.css'
-import {Route, Router, Routes} from "react-router-dom";
-import {Content} from "./Page/Content.tsx";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Content } from "./Page/Content.tsx";
 import NavBar from "./Components/NavBar.tsx";
-import {Gallery} from "./Page/Gallery.tsx";
-import {Booking} from "./Page/Booking.tsx";
-import {Contact} from "./Page/Contact.tsx";
+import { Gallery } from "./Page/Gallery.tsx";
+import { Booking } from "./Page/Booking.tsx";
+import { Contact } from "./Page/Contact.tsx";
+import { Services } from "./Page/Services.tsx";
+import { TermsConditions } from "./Page/Terms&Conditons.tsx";
 
 function App() {
-
-  return (
-    <>
-        <Router>
-            <NavBar>
-                  <Routes>
-                    <>
-                            <Route path="/" element={<Content/> }/>
-                            <Route path="/gallery" element={<Gallery/> }/>
-                            <Route path="/booking" element={<Booking/> }/>
-                            <Route path="/contact" element={<Contact/> }/>
-                            <Route path="/services" element={}/>
-                    </>
-                  </Routes>
-            </NavBar>
-        </Router>
-    </>
-  )
+    return (
+        <>
+            <Router>
+                <NavBar />
+                <Routes>
+                    <Route path="/" element={<Content />} />
+                    <Route path="/gallery" element={<Gallery />} />
+                    <Route path="/booking" element={<Booking />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/services" element={<Services />} />
+                    <Route path="/rules" element={<TermsConditions />} />
+                </Routes>
+            </Router>
+        </>
+    )
 }
 
 export default App
--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import {useNavigate} from "react-router-dom";
+import '../CSS/Navbar.css'
 
 const NavBar:React.FC = () =>{
         const navigate =useNavigate();
