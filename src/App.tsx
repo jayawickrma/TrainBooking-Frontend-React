edb83089146a219cb8cf5ce6816670341445288b
jayawickrama <nishantharaka2004@gmail.com>
2025-03-19 23:15:43 +0530

    creating navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import './App.css'
+import {Route, Router, Routes} from "react-router-dom";
+import {Content} from "./Page/Content.tsx";
+import NavBar from "./Components/NavBar.tsx";
+import {Gallery} from "./Page/Gallery.tsx";
 
 function App() {
 
   return (
     <>
-
+        <Router>
+            <NavBar>
+                  <Routes>
+                    <>
+                            <Route path="/" element={<Content/> }/>
+                            <Route path="/gallery" element={<Gallery/> }/>
+                    </>
+                  </Routes>
+            </NavBar>
+        </Router>
     </>
   )
 }
---
+++ src/Components/NavBar.tsx
@@ -0,0 +1,50 @@
+import * as React from "react";
+import {useNavigate} from "react-router-dom";
+
+const NavBar:React.FC = () =>{
+        const navigate =useNavigate();
+
+        const menuItems = [
+            {
+                    name :'Welcome',
+                    path :'/'
+            },
+            {
+                    name :'Gallery',
+                    path: '/gallery'
+            },
+            {
+                    name :'Booking',
+                    path :'/booking'
+            },
+            {
+                    name :'Contact',
+                    path :'/contact'
+            },
+            {
+                    name :'Services',
+                    path : '/services'
+            },
+            {
+                    name :'Terms & Conditions',
+                    path :'/rules'
+            }
+    ];
+        return(
+            <>
+                    <div className="navbar">
+                            <ul className="navbar-menu">
+                                {menuItems.map((item)=>(
+                                    <li key={item.path}
+                                        className="sidebar-item"
+                                        onClick={()=>navigate(item.path)}
+                                    >
+                                        <span className="navbar-textcr">{item.name}</span>
+                                    </li>
+                                ))}
+                            </ul>
+                    </div>
+            </>
+        )
+}
+export default NavBar
