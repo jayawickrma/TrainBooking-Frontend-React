debe874bb3b6b9e68510c1f96db5c25f5f389120
jayawickrama <nishantharaka2004@gmail.com>
2025-03-28 01:02:00 +0530

    created getAll Schedule in admin panel

--- src/AdminPannel/Components/AdminSchedulePanel.tsx
+++ src/AdminPannel/Components/AdminSchedulePanel.tsx
@@ -1,7 +1,21 @@
-export function AdminSchedulePanel(){
-    return(
-        <>
-        <h1>admin schedule panel</h1>
-        </>
-    )
+import  { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import {
+    getAllSchedules,
+} from '../../Slice/ScheduleSlice'; // Adjust import path as needed
+import ScheduleComponent from "../../Components/ScheduleComponent.tsx"; // Adjust import path as needed
+
+export function AdminSchedulePanel() {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        // @ts-ignore
+        dispatch(getAllSchedules());
+    }, [dispatch]);
+
+    return (
+        <div className="schedule-container">
+                <ScheduleComponent/>
+        </div>
+    );
 }
